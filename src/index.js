7cdc6e200dfc5c905846b09e904e4d19a8102eec
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-04-22 09:07:21 +0700

    reducer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app";
+import { createStore } from "redux";
+import rootReducer from "./components/reducer/root-reducer";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
